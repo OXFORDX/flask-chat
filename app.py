a68f96a130950311ce520d7f8131df6ad0d3e3cc
OXFORDX <volodka.orlov@gmail.com>
2020-01-04 00:42:50 +0200

    Added own tool for uniq tables

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+import datetime
+
 app = Flask(__name__)
 
 # Database path
@@ -13,7 +15,36 @@ socketio = SocketIO(app)
 
 
 class Room(db.Model):
-    room_id = db.Column(db.Integer)
+    room_id = db.Column(db.Integer, primary_key=True)
+    online_users = db.Column(db.String)
+
+
+def uniq_table(tablename):
+    class RoomHistory(db.Model):
+        __tablename__ = tablename
+        id = db.Column(db.Integer, primary_key=True)
+        user = db.Column(db.String)
+        message = db.Column(db.String)
+        date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+
+    RoomHistory.__table__.create(db.session.bind)
+    return RoomHistory
+
+
+def connect_to(tablename):
+    class RoomHistory(db.Model):
+        __tablename__ = tablename
+        id = db.Column(db.Integer, primary_key=True)
+        user = db.Column(db.String)
+        message = db.Column(db.String)
+        date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+
+    return RoomHistory
+
+
+o = connect_to('ROOM123')(user='ok', message='NONOE')
+db.session.add(o)
+db.session.commit()
 
 
 @socketio.on('message')
